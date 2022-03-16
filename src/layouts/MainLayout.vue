6a1bc03bf4a3c9625129f21f52b8f7be8c0105b4
wizard503 <josemdg12@gmail.com>
2022-03-15 18:03:31 -0600

    enlaceComponentes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,6 +13,7 @@
         show-if-above
         :width= "200"
         :breakpoint= "400"
+        elevated
       >
         <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
           <q-list padding>
