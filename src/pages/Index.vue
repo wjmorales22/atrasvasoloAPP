74535eacf2092fbf62eda4f477c8ad85c554990d
Mauricio Israel Menéndez <mauriciomenendez01@gmail.com>
2022-03-16 22:24:25 -0600

    Update Index.vue
    Se agrego un carrusel, y otro tipo de contenidos visual.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,65 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+   <div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+      height="24rem"
+      width="24rem"
     >
-  </q-page>
+      <q-carousel-slide :name="1" img-src="https://www.visitcentroamerica.com/wp-content/uploads/2017/08/ver-centroamerica-el-salvador-santa-ana-03.jpg" />
+      <q-carousel-slide :name="2" img-src="https://hablemosdeculturas.com/wp-content/uploads/2017/12/patrimonio-cultural-de-el-salvador-6-1024x768.jpg" />
+      <q-carousel-slide :name="3" img-src="https://static.elmundo.sv/wp-content/uploads/2020/04/Alcaldia-de-Santa-Ana.jpg" />
+    </q-carousel>
+
+    <div class="row">
+      <div class="col-12 rec">
+        <q-list bordered class="rounded-borders">
+        <q-expansion-item
+          expand-separator
+          icon="explore"
+          label="Destinos"
+          caption="vacio"
+        >
+        </q-expansion-item>
+
+        <q-expansion-item :content-inset-level="0.5" expand-separator icon="person_pin_circle" label="Paradas" caption="vacio">
+
+        </q-expansion-item>
+        <q-expansion-item :content-inset-level="0.5" expand-separator icon="directions_bus" label="Rutas" caption="vacio">
+        </q-expansion-item>
+      </q-list>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      slide: 1,
+      autoplay: true
+    }
+  }
 }
 </script>
+
+<style scoped>
+  .rec{
+    border: 1px solid black;
+    margin-top: 2em;
+  }
+</style>
