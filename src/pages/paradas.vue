c5c0a8fd9fe28c4f13a81497cc69403f1fb36a18
OscarM16 <oscarlg810@gmail.com>
2022-03-18 18:43:03 -0600

    Inicio de Opcion paradas
    Se inicio el diseño del apartado opcion paradas.

--- src/pages/paradas.vue
+++ src/pages/paradas.vue
@@ -1,7 +1,44 @@
 <template>
-    <div class="q-pa-md">
-        <q-layout view="lHh Lpr lff">
-             <h4>Paradas</h4>
-        </q-layout>
+<div class="q-pa-md">
+    <q-layout view="lHh Lpr lff">
+        <div class="" >
+            <div class="row justify-between">
+
+                <q-parallax src="https://pbs.twimg.com/media/FOK3drJXsAIBeBF?format=png&name=small">
+                    <h1 class="text-white">Basic</h1>
+                </q-parallax>
+
+            </div>
+        </div>
+        <div class="col-12 col-md-auto" >
+            <label class="row justify-center">Paradas cercanas</label>
+        </div>
+        <hr class="bg-primary" style="" />
+        <div class="">
+    <div >
+      <q-intersection
+        v-for="index in 5"
+        :key="index"
+        transition="flip-right"
+        class="example-item"
+      >
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="directions_bus" color="black" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>INSA CALLE PTE</q-item-label>
+            <q-item-label caption lines="3">Destinos: Insa principal, Redondel del palmar</q-item-label>
+          </q-item-section>
+
+          <q-item-section side>
+            <q-icon name="chat_bubble" color="green" />
+          </q-item-section>
+        </q-item>
+      </q-intersection>
     </div>
+  </div>
+    </q-layout>
+</div>
 </template>
