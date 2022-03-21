4583d5530aa508b0b466834bfeb55265bdc109ac
Julio Eduardo Aguilar Garcia <egcponcho@gmail.com>
2022-03-21 12:57:33 -0600

    Importacion del elemento de la lista y creacion de array de paradas como ejemplo

---
+++ src/pages/administracionParadas.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="row d-flex justify-center">
+    <q-list class="col-12 col-sm-10 col-md-8 col-lg-6">
+      <elemento-lista v-for="(parada, index) in paradas" :key="index" :parada="parada" />
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+import elementoLista from '../components/administracion paradas/elementoLista.vue'
+
+export default {
+  name: 'administracionParadas',
+  components: { elementoLista },
+  data: () => ({
+    paradas: [
+      {
+        nombreParada: 'Parada de buses Cantarrana',
+        direccionParada: 'Carretera A Sonsonate, Santa Ana'
+      },
+      {
+        nombreParada: 'Parada de buses INSA',
+        direccionParada: '31 Calle Pte, Santa Ana'
+      },
+      {
+        nombreParada: 'Parada de buses Las Vegas',
+        direccionParada: 'Km 61 - 62, Calle Antigua A San Salvador, Santa Ana'
+      },
+      {
+        nombreParada: 'Parada de buses UNICAES',
+        direccionParada: 'Km 64, Calle Antigua A San Salvador, Santa Ana'
+      }
+    ]
+  })
+}
+</script>
