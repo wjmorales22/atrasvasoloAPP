dd930694086e26f6897c4cde776a0b657e8ac885
OscarM16 <oscarlg810@gmail.com>
2022-03-20 18:34:15 -0600

    Continuacion del Detalle de Paradas
    se agregaron algunos componentes del encabezado y el mapa

--- src/pages/detalleParada.vue
+++ src/pages/detalleParada.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="row">
-        <div class="col-12">
-            <h3>Detalle de Parada de Bus</h3>
+    <div class="container-fluid">
+    <div class="row" style="padding-bottom:10px;border-bottom:5px solid #5900ff">
+      <div class="col-2">
+        <q-btn push color="white" text-color="primary" round icon="arrow_back_ios_new" style="font-size: 15px;"/>
+      </div>
+      <div class="col-8">
+        <q-item-label style="text-align: center;padding-top:10px;font-size:20px">Parada de Bus</q-item-label>
+      </div>
+      <div class="col-2">
+      </div>
+    </div>
+    <div class="col-12 col-md-auto" style="margin-top:10px;">
+        <div class="row justify-between">
+          <q-parallax style="" src="https://i.ibb.co/hDhDtLp/Dise-o-sin-t-tulo-36.png"> </q-parallax>
         </div>
+      </div>
+    <div class="row" style="margin-top:10px; border:5px solid black">
+            como estas
+    </div>
     </div>
 </template>
