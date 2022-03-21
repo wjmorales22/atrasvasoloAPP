4f203f955b07dc9cde91f6ad3d0d11ca02bc863e
OscarM16 <oscarlg810@gmail.com>
2022-03-20 19:34:41 -0600

    Continuacion de actualizacion de componentes
    Actualización de aspectos que llevan la pagina de "Detalle de parada" mejora en algunos aspectos gráficos del encabezado y de algunos apartados en especifico.

--- src/pages/paradas.vue
+++ src/pages/paradas.vue
@@ -1,63 +1,45 @@
 <template>
-  <div class="q-pa-md">
+<div class="q-pa-md">
     <q-layout view="lHh Lpr lff">
-      <div class="col-12 col-md-auto" style="border: solid">
-        <div class="row justify-between">
-          <q-parallax style="" src="https://i.ibb.co/hDhDtLp/Dise-o-sin-t-tulo-36.png"> </q-parallax>
+        <div class="col-12 col-md-auto" style="border: solid">
+            <div class="row justify-between">
+                <q-parallax style="" src="https://i.ibb.co/hDhDtLp/Dise-o-sin-t-tulo-36.png"> </q-parallax>
+            </div>
         </div>
-      </div>
-      <div class="col-12 col-md-auto" style="padding-top: 10px">
-        <label
-          style="font-family: 'Bebas Neue', cursive; font-size: 30px"
-          class="row justify-center"
-          >Paradas cercanas</label
-        >
-      </div>
-      <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4">
-          <hr class="bg-primary" style="border-width: 1.5px" />
+        <div class="col-12 col-md-auto" style="padding-top: 10px">
+            <label style="font-family: 'Bebas Neue', cursive; font-size: 30px" class="row justify-center">Paradas cercanas</label>
         </div>
-        <div class="col-4"></div>
-      </div>
-      <div class="row-12">
-        <div>
-          <q-intersection
-            v-for="index in 5"
-            :key="index"
-            transition="flip-right"
-            style="padding-top: 5px"
-            class="example-item"
-          >
-            <q-item clickable v-ripple to="/detalleparada">
-              <q-item-section avatar>
-                <q-icon name="directions_bus" color="black" />
-              </q-item-section>
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4">
+                <hr class="bg-primary" style="border-width: 1.5px" />
+            </div>
+            <div class="col-4"></div>
+        </div>
+        <div class="row-12">
+            <q-intersection v-for="index in 5" :key="index" transition="flip-right" style="padding-top: 5px" class="example-item col-12">
+                <q-item clickable v-ripple to="/detalleparada">
+                    <q-item-section avatar>
+                        <q-icon name="directions_bus" color="black" />
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>INSA CALLE PTE</q-item-label>
+                        <q-item-label caption lines="3">Destinos: Insa principal, Redondel del palmar</q-item-label>
+                    </q-item-section>
+                    <q-item-section side>
+                        <q-icon name="arrow_forward_ios" color="primary" />
+                    </q-item-section>
+                </q-item>
+            </q-intersection>
+        </div>
 
-              <q-item-section>
-                <q-item-label>INSA CALLE PTE</q-item-label>
-                <q-item-label caption lines="3"
-                  >Destinos: Insa principal, Redondel del palmar</q-item-label
-                >
-              </q-item-section>
-
-              <q-item-section side>
-                <q-icon name="arrow_forward_ios" color="primary" />
-              </q-item-section>
-            </q-item>
-          </q-intersection>
+        <div class="row" style="margin-top: 15px">
+            <div class="col-12">
+                <q-img src="https://www.superselectos.com/LandingProductos/GetImagen/114?ancho=1349&alto=337&calidad=75" :ratio="300 / 16" />
+            </div>
         </div>
-      </div>
-      <div class="row" style="margin-top: 15px">
-        <div class="col-12">
-          <q-img
-            src="https://www.superselectos.com/LandingProductos/GetImagen/114?ancho=1349&alto=337&calidad=75"
-            :ratio="300 / 16"
-          />
-        </div>
-      </div>
     </q-layout>
-  </div>
+</div>
 </template>
 
 <style>
