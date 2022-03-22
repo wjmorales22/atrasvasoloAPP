2619071ba7b39eab42c499ae6c1a048695cacebd
Melvin González <mgonzalezramos21@gmail.com>
2022-03-22 13:21:18 -0600

    Update administrarUsuarios.vue
    Se agregarón nuevas caracterirticas en usuarios públicos + comentarios código

--- src/pages/administrarUsuarios.vue
+++ src/pages/administrarUsuarios.vue
@@ -1,19 +1,20 @@
 <template>
   <q-layout view="lHh Lpr lff">
-
+    <!-- Encabezado + btn "Agregar" -->
     <div class="q-pa-md doc-container">
       <div class="row justify-center">
         <div class="col-4">
           <span class="text-weight-bolder text-h5">Administrar Usuarios</span>
         </div>
-        <div class="col-4">
+        <div class="col-2">
           <div class="col" text-xs-right>
-            <q-btn color="primary" icon="style" label="Agregar" />
+            <q-btn color="primary" icon="class" label="Agregar" />
           </div>
         </div>
       </div>
     </div>
 
+    <!-- Titulo "Usuarios colaboradores" + buscador -->
     <q-intersection>
       <q-item>
         <q-item-section avatar>
@@ -23,7 +24,7 @@
           <span class="text-weight-bolder text-h6">Usuarios colaboradores</span>
         </q-item-section>
         <q-item-section side>
-          <q-input filled placeholder="Buscar Usuario">
+          <q-input filled placeholder="Buscar colaborador">
             <template v-slot:append>
               <q-icon name="search" />
             </template>
@@ -32,6 +33,7 @@
       </q-item>
     </q-intersection>
 
+    <!-- Nombre de usuarios colaboradores + opciones "borrar" "editar" -->
     <div class="q-pa-md">
       <q-intersection>
         <q-item clickable v-ripple to="#">
@@ -39,18 +41,23 @@
             <q-icon name="perm_identity" color="black" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Juanastacio Primero</q-item-label>
+            <q-item-label>Juanastacio Primordial</q-item-label>
           </q-item-section>
           <q-item-section side>
-            <q-icon name="arrow_forward_ios" color="primary" />
-          </q-item-section>
-
-          <!-- Combobox para usuarios colaboradores -->
-          <q-item-section>
-            <div class="q-pa-md" style="max-width: 300px">
-              <div class="q-gutter-md">
-                <q-select v-model="model" :options="options" label="Standard" />
-              </div>
+            <div class="text-grey-8 q-gutter-xs">
+              <q-btn class="gt-xs" size="14px" flat dense round icon="delete">
+                <q-tooltip content-class="bg-negative">Borrar</q-tooltip>
+              </q-btn>
+              <q-btn
+                class="gt-xs"
+                size="14px"
+                flat
+                dense
+                round
+                icon="ion-create"
+              >
+                <q-tooltip content-class="bg-indigo">Editar</q-tooltip>
+              </q-btn>
             </div>
           </q-item-section>
         </q-item>
@@ -65,7 +72,21 @@
             <q-item-label>Mauricio Foden</q-item-label>
           </q-item-section>
           <q-item-section side>
-            <q-icon name="arrow_forward_ios" color="primary" />
+            <div class="text-grey-8 q-gutter-xs">
+              <q-btn class="gt-xs" size="14px" flat dense round icon="delete">
+                <q-tooltip content-class="bg-negative">Borrar</q-tooltip>
+              </q-btn>
+              <q-btn
+                class="gt-xs"
+                size="14px"
+                flat
+                dense
+                round
+                icon="ion-create"
+              >
+                <q-tooltip content-class="bg-indigo">Editar</q-tooltip>
+              </q-btn>
+            </div>
           </q-item-section>
         </q-item>
       </q-intersection>
@@ -79,7 +100,21 @@
             <q-item-label>Pedro Parker</q-item-label>
           </q-item-section>
           <q-item-section side>
-            <q-icon name="arrow_forward_ios" color="primary" />
+            <div class="text-grey-8 q-gutter-xs">
+              <q-btn class="gt-xs" size="14px" flat dense round icon="delete">
+                <q-tooltip content-class="bg-negative">Borrar</q-tooltip>
+              </q-btn>
+              <q-btn
+                class="gt-xs"
+                size="14px"
+                flat
+                dense
+                round
+                icon="ion-create"
+              >
+                <q-tooltip content-class="bg-indigo">Editar</q-tooltip>
+              </q-btn>
+            </div>
           </q-item-section>
         </q-item>
       </q-intersection>
@@ -87,6 +122,7 @@
 
     <q-intersection>
       <q-item>
+    <!-- Titulo "Usuarios públicos" + buscador -->
         <q-item-section avatar>
           <q-icon name="manage_accounts" color="black" />
         </q-item-section>
@@ -95,7 +131,7 @@
         </q-item-section>
 
         <q-item-section side>
-          <q-input filled placeholder="Buscar Usuario">
+          <q-input filled placeholder="Buscar usuario público">
             <template v-slot:append>
               <q-icon name="search" />
             </template>
@@ -112,5 +148,92 @@
         -->
       </q-item>
     </q-intersection>
+
+<!-- Nombre de usuarios colaboradores + opciones "borrar" "editar" -->
+    <div class="q-pa-md">
+      <q-intersection>
+        <q-item clickable v-ripple to="#">
+          <q-item-section avatar>
+            <q-icon name="perm_identity" color="black" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Camilo Octavo</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <div class="text-grey-8 q-gutter-xs">
+              <q-btn class="gt-xs" size="14px" flat dense round icon="delete">
+                <q-tooltip content-class="bg-negative">Borrar</q-tooltip>
+              </q-btn>
+              <q-btn
+                class="gt-xs"
+                size="14px"
+                flat
+                dense
+                round
+                icon="ion-create"
+              >
+                <q-tooltip content-class="bg-indigo">Editar</q-tooltip>
+              </q-btn>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-intersection>
+
+      <q-intersection>
+        <q-item clickable v-ripple to="#">
+          <q-item-section avatar>
+            <q-icon name="perm_identity" color="black" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Pedro Castro</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <div class="text-grey-8 q-gutter-xs">
+              <q-btn class="gt-xs" size="14px" flat dense round icon="delete">
+                <q-tooltip content-class="bg-negative">Borrar</q-tooltip>
+              </q-btn>
+              <q-btn
+                class="gt-xs"
+                size="14px"
+                flat
+                dense
+                round
+                icon="ion-create"
+              >
+                <q-tooltip content-class="bg-indigo">Editar</q-tooltip>
+              </q-btn>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-intersection>
+
+      <q-intersection>
+        <q-item clickable v-ripple to="#">
+          <q-item-section avatar>
+            <q-icon name="perm_identity" color="black" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Ronaldo Rubio</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <div class="text-grey-8 q-gutter-xs">
+              <q-btn class="gt-xs" size="14px" flat dense round icon="delete">
+                <q-tooltip content-class="bg-negative">Borrar</q-tooltip>
+              </q-btn>
+              <q-btn
+                class="gt-xs"
+                size="14px"
+                flat
+                dense
+                round
+                icon="ion-create"
+              >
+                <q-tooltip content-class="bg-indigo">Editar</q-tooltip>
+              </q-btn>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-intersection>
+    </div>
   </q-layout>
 </template>
