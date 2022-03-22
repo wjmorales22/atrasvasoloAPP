657cecb166d3e005732b4ebb0a2999aa420ce0a0
Mauricio Israel Menéndez <mauriciomenendez01@gmail.com>
2022-03-22 13:01:20 -0600

    Update Index.vue
    se agrego el espacio para las paradas recientes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,13 +13,21 @@
       @mouseleave="autoplay = true"
       height="24rem"
       width="24rem"
+      class="posCar"
     >
       <q-carousel-slide :name="1" img-src="https://www.visitcentroamerica.com/wp-content/uploads/2017/08/ver-centroamerica-el-salvador-santa-ana-03.jpg" />
       <q-carousel-slide :name="2" img-src="https://hablemosdeculturas.com/wp-content/uploads/2017/12/patrimonio-cultural-de-el-salvador-6-1024x768.jpg" />
       <q-carousel-slide :name="3" img-src="https://static.elmundo.sv/wp-content/uploads/2020/04/Alcaldia-de-Santa-Ana.jpg" />
     </q-carousel>
-
     <div class="row">
+      <div class="col-6 favorite">
+        <q-item-label>Favoritos</q-item-label>
+      </div>
+      <div class="col-6 favorite">
+        <div class="posBtn">
+          <q-btn align="right" class="btn-fixed-width" color="indigo" label="Agregar" size="9px"/>
+        </div>
+      </div>
       <div class="col-12 rec">
         <q-list bordered class="rounded-borders">
         <q-expansion-item
@@ -39,7 +47,54 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
+      <q-item-label>Recientes</q-item-label>
+      <div class="col-12 rec">
+        <q-list bordered class="rounded-borders">
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label lines="1">INSA CALLE PTE</q-item-label>
+          <q-item-label caption lines="2">
+
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+
+      <q-separator inset="item" />
+
+      <q-item clickable v-ripple>
+
+        <q-item-section>
+          <q-item-label lines="1">UES</q-item-label>
+          <q-item-label caption lines="2">
+
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+      <q-separator inset="item" />
+
+      <q-item clickable v-ripple>
+
+        <q-item-section>
+          <q-item-label lines="1">INSA CALLE PTE</q-item-label>
+          <q-item-label caption lines="2">
+
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          5 min ago
+        </q-item-section>
+      </q-item>
+    </q-list>
       </div>
     </div>
   </div>
@@ -60,6 +115,17 @@ export default {
 <style scoped>
   .rec{
     border: 1px solid black;
-    margin-top: 2em;
+    margin-top: 5px;
+  }
+  .favorite{
+    margin-top: 1.5em;
+    margin-bottom: 0.5px;
+  }
+  .posBtn{
+    text-align: right;
+  }
+  .posCar{
+    position: relative;
+    z-index: 1;
   }
 </style>
